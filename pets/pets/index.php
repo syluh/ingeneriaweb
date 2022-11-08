ea395370be3a921bae13306f7422d28fb0db6baa
vecarrion <73541103+syluh@users.noreply.github.com>
2022-11-08 12:00:18 -0500

    Add files via upload
    8 de nov

---
+++ pets/pets/index.php
@@ -0,0 +1,99 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Happy Pets</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+	<link rel="icon" href="images/logo.png" sizes="32x32" />
+    <link rel="icon" href="images/logo.png" sizes="192x192" />
+    <link rel="apple-touch-icon" href="images/logo.png" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
+</head>
+<body>
+<div class="contenedor">
+	<header class="cabeceraPrincipal">
+		<section class="logotipo"><img src="images/logotipoPet.png"></section>
+		<nav class="menuPrincipal">
+			<a href="#">Inicio</a>
+			<a href="#">Servicios</a>
+			<a href="#">Productos</a>
+			<a href="#">Adopción</a>
+			<a href="#">Contactos</a>
+		</nav>
+	</header>
+	<section class="sliderPrincipal">
+		<img src="images/dogs.jpg">
+	</section>
+	<section class="titulo">
+		<h2>Adopcion</h2>
+	</section>
+	<section class="adopcion1">
+		<img src="images/dog1.jpg">
+		<section class="cardog">
+			<h3>firulais</h3>
+			<p>El perro,llamado perro doméstico o can, y en algunos lugares coloquialmente llamado chucho,  tuso,  choco,  entre otros; es un mamífero carnívoro de la familia de los cánidos, que constituye una especie del género Canis.
+			</p>
+		</section>
+	</section>
+	<section class="adopcion2">
+		<img src="images/cat.jpg">
+		<section class="carcat">
+			<h3>Michi</h3>
+			<p>
+				El gato doméstico, llamado más comúnmente gato, y de forma coloquial minino,  michino,  michi,  micho,  mizo,  miz, o morrongo,  y algunos nombres más, es un mamífero carnívoro de la familia Felidae. 
+			</p>
+		</section>
+	</section>
+
+	<section class="titulo1">
+		<h2>Doctores</h2>
+	</section>
+
+	<section class="doctores">
+		<section class="doctor1">
+		<img src="images/doctor1.jpg">
+		<section class="doc1">
+			<h3>Jhonny Sins</h3>
+			<p>
+				Quizás el médico más famoso en Medicina es quizás poco conocido para el gran público, pero a Sir William Osler se le considera el padre de la práctica clínica moderna. 
+			</p>
+		</section>
+	</section>
+	<section class="doctor2">
+		<img src="images/doctor2.jpg">
+		<section class="doc2">
+			<h3>Hipocrates</h3>
+			<p>
+				Nacido en el 460 a.C. a Hipócrates se le llama el padre de la medicina occidental. Suyo es el juramento hipocrático que a día de hoy sigue siendo el referente ético de la profesión.  Aparte de ello.
+			</p>
+		</section>
+	</section>
+	<section class="doctor3">
+		<img src="images/doctor3.jpg">
+		<section class="doc3">
+			<h3>Ignaz Semmelweis</h3>
+			<p>
+				El médico húngaro Ignaz Semmelweis se hizo famoso en el siglo XIX por establecer unas nuevas condiciones sanitarias en el quirófano y en el entorno sanitario. 
+			</p>
+		</section>
+	</section>
+	</section>
+	<section class="sponsor">
+		<h3>Sponsor</h3>
+	</section>
+	<footer class="piePagina">
+		<section class="derechos">
+			<h6>Derechos Reservados UTPL 2022</h6>
+		</section>
+		<nav class="redes">
+			<a href=""><i class="fa-brands fa-facebook"></i></a>
+			<a href=""><i class="fa-brands fa-instagram"></i></a>
+			<a href=""><i class="fa-brands fa-tiktok"></i></a>
+		</nav>
+	</footer>
+</div>
+</body>
+</html>
